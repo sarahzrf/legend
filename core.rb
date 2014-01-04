473eb1644fd2800c8f8783fbcd19e3e76ad02be0
benzrf <benzrf@benzrf.com>
2014-01-03 21:15:51 -0500

    core.rb now exists; it contains basic definitions for the classes World and Room.

---
+++ core.rb
@@ -0,0 +1,69 @@
+require 'singleton'
+
+class World
+	def initialize(rooms={})
+		@rooms = {}
+		@rooms.merge! rooms
+		@current_coords = 0, 0
+	end
+
+	attr_reader :rooms
+	attr_accessor :current_coords
+
+	def current_room
+		@rooms[@current_coords]
+	end
+
+	def add_room(coords, room)
+		@rooms[coords] = room
+	end
+
+	def all_sprites
+		@rooms.values.map &:sprites
+	end
+
+	alias_method :<<, :add_room
+end
+
+class Room
+	def initialize(grid=[[]], sprites=[])
+		@grid = grid
+		@sprites = []
+	end
+
+	attr_reader :sprites
+
+	def add_sprite(sprite)
+		@sprites << sprite
+	end
+
+	alias_method :<<, :add_sprite
+
+	def [] x=nil, y=nil
+		if x and y
+			@grid[x][y]
+		elsif x
+			@grid[x].dup
+		elsif y
+			@grid.transpose[y]
+		else
+			@grid.map &:dup
+		end
+	end
+
+	def []= x=nil, y=nil, val
+		if x and y
+			@grid[x][y] = val
+		elsif x
+			@grid[x] = val
+		elsif y
+			@grid.each {|col| col[y] = val}
+			val
+		else
+			@grid = val
+		end
+	end
+end
+
+# vim:set tabstop=2 shiftwidth=2 noexpandtab:
+
